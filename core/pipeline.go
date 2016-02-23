0ee66fbab9f95beb861255641fb89c8c2b7aec82
378999587@qq.com <378999587@qq.com>
2016-02-23 16:35:02 +0800

    go spider

--- core/pipeline.go
+++ core/pipeline.go
@@ -1,5 +1,10 @@
 package core
 
+import (
+	"fmt"
+	"sync/atomic"
+)
+
 // 条目
 type Item map[string]interface{}
 
@@ -18,20 +23,57 @@ type ProcessItem func(item Item) (result Item, err error)
 
 // 条目处理管道
 type ItemPipeline interface {
-	// 发送条目。
 	Send(item Item) []error
-	// FailFast方法会返回一个布尔值。该值表示当前的条目处理管道是否是快速失败的。
-	// 这里的快速失败是指：只要对某个条目的处理流程在某一个步骤上出错，
-	// 那么条目处理管道就会忽略掉后续的所有处理步骤并报告错误。
-	FailFast() bool
-	// 设置是否快速失败。
-	SetFailFast(failFast bool)
-	// 获得已发送、已接受和已处理的条目的计数值。
-	// 更确切地说，作为结果值的切片总会有三个元素值。这三个值会分别代表前述的三个计数。
 	Count() []uint64
-	// 获取正在被处理的条目的数量。
 	ProcessingNumber() uint64
-	// 获取摘要信息。
 	Summary() string
 }
+
+func NewItemPipeline(itemProcessors []ProcessItem) ItemPipeline {
+	if itemProcessors == nil {
+		panic(fmt.Errorf("Invalid item processor list!"))
+	}
+	innerItemProcessors := make([]ProcessItem, 0)
+	for i, ip := range itemProcessors {
+		if ip == nil {
+			panic(fmt.Errorf("Invalid item processor[%d]!\n", i))
+		}
+		innerItemProcessors = append(innerItemProcessors, ip)
+	}
+	return &myItemPipeline{itemProcessors: innerItemProcessors}
+}
+
+type myItemPipeline struct {
+	itemProcessors   []ProcessItem // 条目处理器的列表。
+	sent             uint64        // 已被发送的条目的数量。
+	accepted         uint64        // 已被接受的条目的数量。
+	processed        uint64        // 已被处理的条目的数量。
+	processingNumber uint64        // 正在被处理的条目的数量。
+}
+
+func (this *myItemPipeline) Send(item Item) []error {
+	return nil
+}
+
+func (this *myItemPipeline) Count() []uint64 {
+	counts := make([]uint64, 3)
+	counts[0] = atomic.LoadUint64(&this.sent)
+	counts[1] = atomic.LoadUint64(&this.accepted)
+	counts[2] = atomic.LoadUint64(&this.processed)
+	return counts
+}
+
+func (this *myItemPipeline) ProcessingNumber() uint64 {
+	return atomic.LoadUint64(&this.processingNumber)
+}
+
+func (this *myItemPipeline) Summary() string {
+	counts := this.Count()
+	summary := fmt.Sprintf("processorNumber: %d, sent: %d, accepted: %d, processed: %d, processing: %d", len(this.itemProcessors), counts[0], counts[1], counts[2], this.ProcessingNumber())
+
+	return summary
+}
+
+
+
 
